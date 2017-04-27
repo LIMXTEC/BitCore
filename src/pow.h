039f160a5ad29582d39a7229e902d3bf97c60a27
Chris <apfelbaum@email.de>
2017-04-27 18:46:13 +0200

    Update pow.h

--- src/pow.h
+++ src/pow.h
@@ -16,8 +16,7 @@ class uint256;
 
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params&); // BitCore: Select retargeting
 
-unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const CBlockHeader *pblock, const Consensus::Params&); // For Setion test
-
+unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params&);
 /** Check whether a block hash satisfies the proof-of-work requirement specified by nBits */
 bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&);
 
