7bfd42cc3e6a9bec29d429d75652a5d84d06e800
Christian Knöpke <apfelbaum@email.de>
2017-09-23 20:28:05 +0200

    Revert "Copyright Changes Bitcore -> Bitcoin Add new line"
    This reverts commit 2b53eeef0ff2ff3d2a45cea5d48d8a0073dff830.

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -1,6 +1,5 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2017-2017 The BitCoin Core developers
-//// Copyright (c) 2009-2017 The BitCore Core developers
+// Copyright (c) 2009-2017 The BitCore Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
