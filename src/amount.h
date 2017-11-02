d9663bb4b3a8a1a520f06c26d7b72bc7c873c30e
Chris <apfelbaum@email.de>
2017-11-02 21:55:26 +0100

    Change MAX_MONEY security function.
    That has not to do with the coin rollout.

--- src/amount.h
+++ src/amount.h
@@ -23,13 +23,13 @@ extern const std::string CURRENCY_UNIT;
 /** No amount larger than this (in satoshi) is valid.
  *
  * Note that this constant is *not* the total money supply, which in BitCore
- * currently happens to be less than 35000000 BTX for various reasons, but
+ * currently happens to be less than 21000000 BTX for various reasons, but
  * rather a sanity check. As this sanity check is used by consensus-critical
  * validation code, the exact value of the MAX_MONEY constant is consensus
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
-static const CAmount MAX_MONEY = 35000000 * COIN; // BitCore 35000000
+static const CAmount MAX_MONEY = 21000000 * COIN; // BitCore 21000000
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 /**
