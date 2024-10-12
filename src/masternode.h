0a856077d0f3d4e432a6bead242cdb79e70e97c6
limxdev_chris <10200540+limxdev@users.noreply.github.com>
2024-10-13 00:36:50 +0200

    Update masternode.h

--- src/masternode.h
+++ src/masternode.h
@@ -17,9 +17,9 @@ class CConnman;
 static const int MASTERNODE_CHECK_SECONDS               =   5;
 static const int MASTERNODE_MIN_MNB_SECONDS             =   5 * 60;
 static const int MASTERNODE_MIN_MNP_SECONDS             =  10 * 60;
-static const int MASTERNODE_EXPIRATION_SECONDS          =   4 * 65 * 60;  //BTX 2024-10 We make it a little easier to keep the mn online. old 65 * 60; 
+static const int MASTERNODE_EXPIRATION_SECONDS          =   4 * 60 * 60;  //BTX 2024-10 We make it a little easier to keep the mn online. old 65 * 60; 
 static const int MASTERNODE_WATCHDOG_MAX_SECONDS        = 120 * 60;
-static const int MASTERNODE_NEW_START_REQUIRED_SECONDS  =   4 * 180 * 60;  // old 180 * 60;
+static const int MASTERNODE_NEW_START_REQUIRED_SECONDS  =   3 * 24 * 60 * 60;  // old 180 * 60;
 
 static const int MASTERNODE_POSE_BAN_MAX_SCORE          = 5;
 
